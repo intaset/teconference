a9178f1d3213571372db323ae95e45af190ed720
Mansura Chowdhury <c.mansura@gmail.com>
2016-04-08 13:34:08 -0400

    updated website to 2017

--- _site/contactus.php
+++ _site/contactus.php
@@ -123,7 +123,7 @@ mail($my_email,$subject,$message,$headers);
 <meta name="robots" content="noarchive">
 <meta name="description" content="">
 <meta name="keywords" content="">
-<title>ICTE'16 - Contact Us</title>
+<title>ICTE'17 - Contact Us</title>
 
 <meta name="handheldfriendly" content="true">
 <meta name="mobileoptimized" content="240">
@@ -152,7 +152,7 @@ mail($my_email,$subject,$message,$headers);
 
 <body>
 <nav id="slide-menu">
-  <h1>ICTE'16</h1>
+  <h1>ICTE'17</h1>
   <ul>
     <li><a href="/">Home</a></li>
     <li><a href="../papers">Paper Submissions</a></li>
@@ -172,30 +172,30 @@ mail($my_email,$subject,$message,$headers);
 <div id="content">
   <div class="desktop">
   <div class="cbp-af-header">
-	<div class="cbp-af-inner">
-		<a href="/"><img src="../img/logo.png" class="flex-logo"></a>
-			<nav>
-				<a href="/">Home</a><p class="dot">&middot;</p><a href="../papers">Paper Submission</a><p class="dot">&middot;</p><a href="../program">Program</a><p class="dot">&middot;</p><a href="../dates">Important Dates</a><p class="dot">&middot;</p><a href="../registration">Registration</a><p class="dot">&middot;</p><a href="../committee">Committee</a><p class="dot">&middot;</p><a href="../keynote">Keynotes</a><p class="dot">&middot;</p><a href="../sponsor">Sponsors</a><p class="dot">&middot;</p><a href="../venue">Venue</a><p class="dot">&middot;</p><a href="../accommodation">Accommodation</a><p class="dot">&middot;</p><a href="../symposium">Symposiums</a><p class="dot">&middot;</p><a href="#contact">Contact Us</a>
-		</nav>
-	</div>
+  <div class="cbp-af-inner">
+    <a href="/"><img src="../img/logo.png" class="flex-logo"></a>
+      <nav>
+        <a href="/">Home</a><p class="dot">&middot;</p><a href="../papers">Paper Submission</a><p class="dot">&middot;</p><a href="../program">Program</a><p class="dot">&middot;</p><a href="../dates">Important Dates</a><p class="dot">&middot;</p><a href="../registration">Registration</a><p class="dot">&middot;</p><a href="../committee">Committee</a><p class="dot">&middot;</p><a href="../keynote">Keynotes</a><p class="dot">&middot;</p><a href="../sponsor">Sponsors</a><p class="dot">&middot;</p><a href="../venue">Venue</a><p class="dot">&middot;</p><a href="../accommodation">Accommodation</a><p class="dot">&middot;</p><a href="../symposium">Symposiums</a><p class="dot">&middot;</p><a href="#contact">Contact Us</a>
+    </nav>
+  </div>
 </div>
 </div>
 
   <header>
     <div class="mobile">
       <div class="cbp-af-header">
-	<div class="cbp-af-inner">
-		<div class="unit unit-s-3-4 unit-m-1-3 unit-l-1-3">
-      		<a href="/"><img src="../img/logo.png" class="flex-logo"></a>
-   	 	</div>
-    	<div class="unit unit-s-1-3 unit-m-2-3 unit-m-2-3-1 unit-l-2-3">
-      		<div class="menu-trigger"></div>
-  		</div>
-	</div>
+  <div class="cbp-af-inner">
+    <div class="unit unit-s-3-4 unit-m-1-3 unit-l-1-3">
+          <a href="/"><img src="../img/logo.png" class="flex-logo"></a>
+      </div>
+      <div class="unit unit-s-1-3 unit-m-2-3 unit-m-2-3-1 unit-l-2-3">
+          <div class="menu-trigger"></div>
+      </div>
+  </div>
 </div>
         <div class="bg">
-          <h1>International Conference on<br>Traffic Engineering (ICTE'16)</h1>
-          <p class="subhead">March 30 - 31, 2016 | Prague, Czech Republic</p>
+          <h1>2<sup>nd</sup> International Conference on<br>Traffic Engineering (ICTE'17)</h1>
+          <p class="subhead">April 3 - 4, 2017 | Barcelona, Spain</p>
 
           <a href="../papers" class="bg-link">Paper Submission</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -220,8 +220,8 @@ mail($my_email,$subject,$message,$headers);
         </div>
 
         <div class="bg">
-          <h1>International Conference on<br>Traffic Engineering (ICTE'16)</h1>
-          <p class="subhead">March 30 - 31, 2016 | Prague, Czech Republic</p>
+          <h1>2<sup>nd</sup> International Conference on<br>Traffic Engineering (ICTE'17)</h1>
+          <p class="subhead">April 3 - 4, 2017 | Barcelona, Spain</p>
 
           <a href="../papers" class="bg-link">Paper Submission</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -248,15 +248,22 @@ mail($my_email,$subject,$message,$headers);
       <div id="main-slider" class="liquid-slider">
     <div>
       <h2 class="title">1</h2>
-      <p class="bold">ICTE 2016:</p>
-      <p class="body">ICTE 2016 will  be held in Ottawa, Canada on March 30 - 31, 2016.</p>
+      <p class="bold">ICTE 2017:</p>
+      <p class="body">ICTE 2017 will  be held in Barcelona, Spain on April 3 - 4, 2017.</p>
+
+      <p class="bold">Poster Board Dimensions:</p>
+      <p class="body">Authors presenting via poster boards are to be informed that poster boards are 110 cm height and 80 cm width.</p>
     </div>          
     <div>
       <h2 class="title">2</h2>
       <p class="bold">Best Paper Award:</p>
       <p class="body">Two best paper awards will be conferred to author(s) of the papers that receive the highest rank during the peer-review and by the respected session chairs. Please visit <a href="../papers" class="body-link">Paper Submission</a> for more information.</p>
     </div>
-
+    <div>
+      <h2 class="title">3</h2>
+      <p class="bold">Propose Exhibits, Workshops &amp; More</p>
+      <p class="body">ICTE attracts a wide range of researchers in the field of traffic engineering. As a prominent company in the field of traffic engineering, we would like to offer you an exhibit at ICTE. Please visit <a href="../events" class="body-link">Events</a> for more information.</p>
+    </div>
   </div>
     </div>
   </div>
@@ -264,68 +271,68 @@ mail($my_email,$subject,$message,$headers);
 <div class="unit unit-s-1 unit-m-1-4-1 unit-l-1-4-1">
   <div class="unit-spacer content">
     <p class="body">We have received your message and we will try our best to get back to you within the next 48 hours.<br><br>
-    Thank you for your interest in ICTE'16.</p>
+    Thank you for your interest in ICTE'17.</p>
   </div>
 </div>
 
   <div class="unit unit-s-1 unit-m-1-3-1 unit-l-1-3-1">
   <div class="unit-spacer">
     <section class="main">
-				<div class="custom-calendar-wrap">
-					<div id="custom-inner" class="custom-inner">
-						<div class="custom-header clearfix">
-							<nav>
-								<span id="custom-prev" class="custom-prev"></span>
-								<span id="custom-next" class="custom-next"></span>
-							</nav>
-							<h2 id="custom-month" class="custom-month"></h2>
-							<h3 id="custom-year" class="custom-year"></h3>
-						</div>
-						<div id="calendar" class="fc-calendar-container"></div>
-					</div>
-				</div>
-			</section>
+        <div class="custom-calendar-wrap">
+          <div id="custom-inner" class="custom-inner">
+            <div class="custom-header clearfix">
+              <nav>
+                <span id="custom-prev" class="custom-prev"></span>
+                <span id="custom-next" class="custom-next"></span>
+              </nav>
+              <h2 id="custom-month" class="custom-month"></h2>
+              <h3 id="custom-year" class="custom-year"></h3>
+            </div>
+            <div id="calendar" class="fc-calendar-container"></div>
+          </div>
+        </div>
+      </section>
     <h2>Upcoming Dates</h2>
 
 <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
-	<div class="date">
-		Dec. 10, 2015
-	</div>
+  <div class="date">
+    Oct. 05, 2016
+  </div>
 </div>
 
 <div class="unit unit-s-1 unit-m-3-4 unit-l-3-4">
-	<div class="unit-spacer">
-		Paper Submission Deadline
-	</div>
+  <div class="unit-spacer">
+    Paper Submission Deadline
+  </div>
 </div>
 </div>
 
 <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
-	<div class="date">
-		Jan. 25, 2016
-	</div>
+  <div class="date">
+    Dec. 10, 2016
+  </div>
 </div>
 
 <div class="unit unit-s-1 unit-m-3-4 unit-l-3-4">
-	<div class="unit-spacer">
-		Notification of Authors
-	</div>
+  <div class="unit-spacer">
+    Notification of Authors
+  </div>
 </div>
 </div>
 
 <div class="grid events">
 <div class="unit unit-s-1 unit-m-1-4 unit-l-1-4">
-	<div class="date">
-		Feb. 20, 2016
-	</div>
+  <div class="date">
+    Feb. 01, 2017
+  </div>
 </div>
 
 <div class="unit unit-s-1 unit-m-3-4 unit-l-3-4">
-	<div class="unit-spacer">
-		Camera Ready Submission Deadline
-	</div>
+  <div class="unit-spacer">
+    Camera Ready Submission Deadline
+  </div>
 </div>
 </div>
   </div>
@@ -333,22 +340,22 @@ mail($my_email,$subject,$message,$headers);
 </div>
 
 <footer id="contact">
-	<div class="grid">
-	<div class="unit unit-s-1 unit-m-1-3 unit-l-1-3">
-	<div class="unit-spacer">
-		<h2>Contact Us</h2>
-		<p class="body">International ASET Inc.<br>
-		Unit No. 417, 1376 Bank St.<br>
-		Ottawa, Ontario, Canada<br>
-		Postal Code: K1H 7Y3<br>
-		+1-613-695-3040<br>
-		<a href="mailto:info@teconference.com">info@teconference.com</a></p>
-		</div>
-	</div>
+  <div class="grid">
+  <div class="unit unit-s-1 unit-m-1-3 unit-l-1-3">
+  <div class="unit-spacer">
+    <h2>Contact Us</h2>
+    <p class="body">International ASET Inc.<br>
+    Unit No. 417, 1376 Bank St.<br>
+    Ottawa, Ontario, Canada<br>
+    Postal Code: K1H 7Y3<br>
+    +1-613-695-3040<br>
+    <a href="mailto:info@teconference.com">info@teconference.com</a></p>
+    </div>
+  </div>
 
-	<div class="unit unit-s-1 unit-m-2-3 unit-l-2-3 contact">
-	<div class="unit-spacer">
-	<p class="body">For questions or comments regarding ICTE'16, please fill out the form below:</p>
+  <div class="unit unit-s-1 unit-m-2-3 unit-l-2-3 contact">
+  <div class="unit-spacer">
+  <p class="body">For questions or comments regarding ICTE'17, please fill out the form below:</p>
 
     <form action="../contactus.php" method="post" enctype="multipart/form-data" name="ContactForm">
   
@@ -398,14 +405,14 @@ mail($my_email,$subject,$message,$headers);
         
 </form>
     </div>
-	</div>
-	</div>
+  </div>
+  </div>
 </footer> 
 
 <div class="copyright">
-	<a href="international-aset.com">International ASET Inc.</a> | <a href="http://international-aset.com/phplistpublic/?p=subscribe&id=1">Subscribe</a> | <a href="../terms">Terms of Use</a> | <a href="../sitemap">Sitemap</a>
-	<p class="body">&copy; Copyright International ASET Inc., 2015. All rights reserved.</p>
-	<p class="copyright1">Have any feedback? Please provide them here: <script>var refURL = window.location.protocol + "//" + window.location.host + window.location.pathname; document.write('<a href="http://international-aset.com/feedback/?refURL=' + refURL+'" class="body-link">Feedback</a>');</script></p>
+  <a href="international-aset.com">International ASET Inc.</a> | <a href="http://international-aset.com/phplistpublic/?p=subscribe&id=1">Subscribe</a> | <a href="../terms">Terms of Use</a> | <a href="../sitemap">Sitemap</a>
+  <p class="body">&copy; Copyright International ASET Inc., 2016. All rights reserved.</p>
+  <p class="copyright1">Have any feedback? Please provide them here: <script>var refURL = window.location.protocol + "//" + window.location.host + window.location.pathname; document.write('<a href="http://international-aset.com/feedback/?refURL=' + refURL+'" class="body-link">Feedback</a>');</script></p>
 </div>
 </div>
 
